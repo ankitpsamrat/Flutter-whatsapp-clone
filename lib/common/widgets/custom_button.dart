597f92cab49f7454d6212a5fb8ccaa97895b99e5
ankitpsamrat <ankit.p.samrat@gmail.com>
2022-08-02 12:25:07 +0530

    first commit

---
+++ lib/common/widgets/custom_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/colors.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final String text;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: blackColor,
+        ),
+      ),
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        primary: tabColor,
+        minimumSize: const Size(double.infinity, 50),
+      ),
+    );
+  }
+}
---
+++ lib/features/landing/screens/landing_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/colors.dart';
+import 'package:whatsapp/common/widgets/custom_button.dart';
+
+class LandingScreen extends StatelessWidget {
+  const LandingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 50),
+            Text(
+              'Welcome to WhatsApp',
+              style: TextStyle(
+                fontSize: 33,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(height: size.height / 9),
+            Image.asset(
+              'assets/images/bg.png',
+              height: 340,
+              width: 340,
+              color: tabColor,
+            ),
+            SizedBox(height: size.height / 9),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Text(
+                'Read our Privacy Policy. Tap "Agree and continue" to accept the Terms of Service.',
+                style: TextStyle(color: grayColor),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(height: 10),
+            SizedBox(
+              width: size.width * 0.75,
+              child: CustomButton(
+                text: 'AGREE AND CONTINUE',
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
